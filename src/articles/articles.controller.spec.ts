5fdf111878384eea8e9f6dadbd7de555c62c4095
Fernando Ibanez <fernando.ibanez@bjss.com>
2022-10-03 16:40:18 +1100

    added some unit tests

--- src/articles/articles.controller.spec.ts
+++ src/articles/articles.controller.spec.ts
@@ -1,13 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesController } from './articles.controller';
+import { ArticlesService } from './articles.service';
+import { v4 as uuidv4 } from 'uuid';
 
 describe('ArticlesController', () => {
   let controller: ArticlesController;
 
+  const mockArticlesService = {
+    insertArticle: jest.fn().mockResolvedValueOnce(uuidv4())
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticlesController],
-    }).compile();
+      providers: [ArticlesService]
+    })
+      .overrideProvider(ArticlesService)
+      .useValue(mockArticlesService)
+      .compile();
 
     controller = module.get<ArticlesController>(ArticlesController);
   });
@@ -15,4 +25,11 @@ describe('ArticlesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should be create an article', () => {
+    expect(controller.addArticle('testName', 'testDescription', 1)).resolves.toEqual({
+      id: expect.any(String)
+    });
+  });
+
 });
